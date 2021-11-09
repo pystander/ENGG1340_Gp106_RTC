ebc7ce11c9afd0e347f63db26b8de3476d2603f8
pystander <67277708+pystander@users.noreply.github.com>
2021-11-09 19:29:43 +0800

    Update consumables.h

--- libs/entities/items/consumables.h
+++ libs/entities/items/consumables.h
@@ -16,4 +16,40 @@ class SmallHpPotion : public GameItem{
         }
 };
 
+class MideumHpPotion : public GameItem{
+    public:
+        MideumHpPotion() : GameItem("Medium HP potion", HEALING, CONSUMABLE){
+            Random rng = Random(25, 40);
+            this->itemStat.healAmount = rng.getInt();
+        };
+
+        GameItem* copy(){
+            return new MideumHpPotion();
+        }
+};
+
+class ExtraHpPotion : public GameItem{
+    public:
+        ExtraHpPotion() : GameItem("Extra HP potion", HEALING, CONSUMABLE){
+            Random rng = Random(50, 75);
+            this->itemStat.healAmount = rng.getInt();
+        };
+
+        GameItem* copy(){
+            return new ExtraHpPotion();
+        }
+};
+
+class PhoenixTear : public GameItem{
+    public:
+        PhoenixTear() : GameItem("Phoenix Tear", HEALING, CONSUMABLE){
+            Random rng = Random(80, 100);
+            this->itemStat.healAmount = rng.getInt();
+        };
+
+        GameItem* copy(){
+            return new PhoenixTear();
+        }
+};
+
 #endif

ace7a14d526dfe5b9debe4adc4b4fc2133b09440
pystander <67277708+pystander@users.noreply.github.com>
2021-11-11 12:17:56 +0800

    Update weaker_enermies.h

--- libs/entities/enemies/weaker_enemies.h
+++ libs/entities/enemies/weaker_enemies.h
@@ -2,6 +2,7 @@
 #define WEAKER_ENEMIES_H
 
 #include "libs/entities/character.h"
+#include "libs/utils/random_util.h"
 
 class Goblin : public GameCharacter{
     public:
@@ -11,9 +12,9 @@ class Goblin : public GameCharacter{
             this->baseStat.phyAttack = 35;
             this->baseStat.phyResist = 2;
 
-            // Loots
-            this->money = 150;
-            this->xp = 50;
+            // Random loots;
+            this->money = Random(30, 50).getInt();
+            this->xp = Random(20, 50).getInt();
 
             // Level
             const int level = 1;
@@ -36,9 +37,9 @@ class Wolf : public GameCharacter{
             this->baseStat.phyAttack = 40;
             this->baseStat.phyResist = 3;
 
-            // Loots
-            this->money = 200;
-            this->xp = 70;
+            // Random loots
+            this->money = Random(70, 120).getInt();
+            this->xp = Random(50, 70).getInt();
 
             // Level
             const int level = 3;
@@ -61,9 +62,9 @@ class DarkFairy : public GameCharacter{
             this->baseStat.magAttack = 50;
             this->baseStat.phyResist = 5;
 
-            // Loots
-            this->money = 250;
-            this->xp = 100;
+            // Random loots
+            this->money = Random(210, 250).getInt();
+            this->xp = Random(60, 90).getInt();
 
             // Level
             const int level = 7;
@@ -83,13 +84,12 @@ class Dryad : public GameCharacter{
         Dryad() : GameCharacter("Dryad", false, MONSTER){
             // Base stats
             this->maxHp = 150;
-            this->currentHp = this->maxHp;
             this->baseStat.phyAttack = 70;
             this->baseStat.phyResist = 7;
 
-            // Loots
-            this->money = 275;
-            this->xp = 120;
+            // Random loots
+            this->money = Random(230, 275).getInt();
+            this->xp = Random(80, 120).getInt();
 
             // Level
             const int level = 9;
@@ -108,13 +108,13 @@ class Zombie : public GameCharacter{
     public:
         Zombie() : GameCharacter("Zombie", false, MONSTER){
             // Base stats
-            this->maxHp = 120;
-            this->baseStat.phyAttack = 50;
+            this->maxHp = 150;
+            this->baseStat.phyAttack = 40;
             this->baseStat.phyResist = 15;
 
             // Loots
-            this->money = 300;
-            this->xp = 150;
+            this->money = Random(250, 290).getInt();
+            this->xp = Random(100, 150).getInt();
 
             // Level
             const int level = 10;
@@ -135,10 +135,11 @@ class Spirit : public GameCharacter{
             this->maxHp = 50;
             this->baseStat.magAttack = 50;
             this->baseStat.phyResist = -1; // Immune to physical attack
+            this->baseStat.magResist = 25;
 
-            // Loots
-            this->money = 50;
-            this->xp = 150;
+            // Random loots
+            this->money = Random(200, 230).getInt();
+            this->xp = Random(90, 120).getInt();
 
             // Level
             for(int i = 0; i < 10; i++)

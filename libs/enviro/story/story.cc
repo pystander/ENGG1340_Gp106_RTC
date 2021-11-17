4aebd90fbe463ca32096ceade81b798864f24bf2
vincentIsGood <vincentdev324@gmail.com>
2021-11-17 12:17:13 +0800

    Removed windows commands

--- libs/enviro/story/story.cc
+++ libs/enviro/story/story.cc
@@ -4,15 +4,14 @@
 #include "libs/enviro/game.h"
 
 void storyLine(Game* game){
-    system("cls");
+    system("clear");
     std::cout << "Once upon a time, somewhere in a land full of monsters, ." << std::endl;
     std::cout << "This land beneath our feet was once stepped by the theree ancient Gods." << std::endl;
     std::cout << "But one day, the Gods suddenly disappeared, and never return." << std::endl;
     std::cout << "Without protection from the Gods, people were miserably ravaged by monsters." << std::endl;
     std::cout << "People did not lose their faith, and waiting for the return of Gods." << std::endl;
-    system("pause");
-    
-    system("cls");
+    getchar();
+    system("clear");
     std::cout << "As the Prophecy foretold:" << std::endl;
     std::cout << "There will be a hero," << std::endl;
     std::cout << "walk through the Road to the Castle." << std::endl;
